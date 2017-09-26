b0dfd084788511a8f2f750a7ed8816ed61cc90d7
Minh Le Tran Ba <baminh.letran@gmail.com>
2017-09-26 17:41:50 +0700

    Modal WIP

--- src/app/public/components/product-details.component.ts
+++ src/app/public/components/product-details.component.ts
@@ -1,22 +1,30 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewEncapsulation } from '@angular/core';
 import { ActivatedRoute } from "@angular/router";
+import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
 
 @Component({
     moduleId: module.id,
     selector: 'app-product-details',
     templateUrl: 'product-details.component.html',
-    styleUrls: ['./product-details.component.scss']
+    styleUrls: ['./product-details.component.scss'],
+    encapsulation: ViewEncapsulation.None,
 })
 export class ProductDetailsComponent implements OnInit {
-  resolvedData;
-  constructor(private route: ActivatedRoute) {
-    this.route.data.subscribe(val => {
-      console.log(val['data'])
-      this.resolvedData = val["data"];
-    });
-  }
+    productDetails;
+    previewProduct;
+    constructor(private route:ActivatedRoute, private modalService:NgbModal) {
+        this.route.data.subscribe(val => {
+            this.productDetails = val["data"];
+        });
+    }
 
-  ngOnInit() {
+    ngOnInit() {
 
-  }
+    }
+
+    open(content, product) {
+        this.previewProduct = product;
+        this.modalService.open(content, {size: 'lg', windowClass: 'quick-view-modal'});
+    }
+
 }
--- src/app/public/services/product-details.service.ts
+++ src/app/public/services/product-details.service.ts
@@ -4,21 +4,123 @@ import { Observable } from "rxjs";
 @Injectable()
 export class ProductDetailsService {
 
-    constructor() { }
+    constructor() {
+    }
 
-    getProductDetails(category: String) {
-      let mockData:string = "";
-      return new Observable(observer => {
-        setTimeout(() => {
-          console.log("Resolved Observable");
-          mockData = "Mock Data String (O)";
-          observer.next(mockData);
-          console.log("Emitted new Observable data");
-          setTimeout(() => {
-            console.log("Completing Observable");
-            observer.complete();
-          }, 1000);
-        }, 2000);
-      });
+    getProductDetails(category) {
+        let mockData = {
+            cloths: {
+                man: [
+                    {
+                        fullName: 'Funky shirt with long name',
+                        shortName: 'Quicksilver',
+                        description: 'A collection of textile samples lay spread out on the table - Samsa was a travelling salesman - and above it there hung a picture that he had recently cut.',
+                        mainImage: '',
+                        extraImages: [
+                            '',
+                            '',
+                            ''
+                        ],
+                        availableSizes: [
+                            'Small',
+                            'Medium',
+                            'Large'
+                        ],
+                        price: 143,
+                        onSale: 15, //discount percentage
+                        isNew: false
+                    },
+                    {
+                        fullName: 'Funky shirt ',
+                        shortName: 'Quick sleeve',
+                        description: 'A collection of textile samples lay spread out on the table - Samsa was a travelling salesman - and above it there hung a picture that he had recently cut.',
+                        mainImage: 'http://hunter.ondrejsvestka.cz/1-2/img/detailbig1.jpg',
+                        extraImages: [
+                            'http://hunter.ondrejsvestka.cz/1-2/img/detailsmall1.jpg',
+                            'http://hunter.ondrejsvestka.cz/1-2/img/detailsmall2.jpg',
+                            'http://hunter.ondrejsvestka.cz/1-2/img/detailsmall3.jpg'
+                        ],
+                        availableSizes: [
+                            'Small',
+                            'Medium',
+                            'Large'
+                        ],
+                        price: 343,
+                        onSale: 15, //discount percentage
+                        isNew: true
+                    },
+                    {
+                        fullName: 'Funky shirt short  name',
+                        shortName: 'Slow sleeve',
+                        description: 'A collection of textile samples lay spread out on the table - Samsa was a travelling salesman - and above it there hung a picture that he had recently cut.',
+                        mainImage: '',
+                        extraImages: [
+                            '',
+                            '',
+                            ''
+                        ],
+                        availableSizes: [
+                            'Small',
+                            'Medium',
+                            'Large',
+                            'X-Large'
+                        ],
+                        price: 243,
+                        onSale: 5, //discount percentage
+                        isNew: true
+                    },
+                    {
+                        fullName: 'Blouse',
+                        shortName: 'Short sleeve',
+                        description: 'A collection of textile samples lay spread out on the table - Samsa was a travelling salesman - and above it there hung a picture that he had recently cut.',
+                        mainImage: '',
+                        extraImages: [
+                            '',
+                            '',
+                            ''
+                        ],
+                        availableSizes: [
+                            'Small',
+                            'Medium',
+                            'Large',
+                            'X-Large'
+                        ],
+                        price: 200,
+                        onSale: 5, //discount percentage
+                        isNew: true
+                    },
+                    {
+                        fullName: 'Shirt',
+                        shortName: 'Long sleeve',
+                        description: 'A collection of textile samples lay spread out on the table - Samsa was a travelling salesman - and above it there hung a picture that he had recently cut.',
+                        mainImage: '',
+                        extraImages: [
+                            '',
+                            '',
+                            ''
+                        ],
+                        availableSizes: [
+                            'Small',
+                            'Medium',
+                            'Large',
+                            'X-Large'
+                        ],
+                        price: 300,
+                        onSale: 5, //discount percentage
+                        isNew: true
+                    },
+                ]
+            }
+
+
+        };
+        return new Observable(observer => {
+            setTimeout(() => {
+                console.log("Resolved Observable");
+                observer.next(mockData.cloths[category]);
+                // Must have complete() for resolver to run properly
+                observer.complete();
+            }, 2000);
+        });
     }
 }
