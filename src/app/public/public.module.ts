69d78d742a0c4a0489453eafeb2e89491d2e7102
Minh Le Tran Ba <baminh.letran@gmail.com>
2017-10-02 17:39:39 +0700

    WIP Product details page

--- src/app/public/public.module.ts
+++ src/app/public/public.module.ts
@@ -7,11 +7,13 @@ import { NavBarComponent } from "../common/components/nav-bar.component";
 import { FooterComponent } from "../common/components/footer.component";
 import { HomeComponent } from "./components/home.component";
 import { WomenClothsComponent } from "./components/women-cloths.component";
-import { ProductDetailsResolver } from "./services/product-details.resolver";
+import { ProductsResolver } from "./services/products.resolver";
 import { ProductDetailsService } from "./services/product-details.service";
 import { ProductsComponent } from "./components/products.component";
 import { NgxPaginationModule } from 'ngx-pagination';
 import { FormsModule } from '@angular/forms';
+import { ProductDetailsResolver } from './services/product-details.resolver';
+import { ProductDetailsComponent } from './components/product-details.component';
 
 @NgModule({
     imports: [
@@ -25,10 +27,11 @@ import { FormsModule } from '@angular/forms';
         HomeComponent,
         WomenClothsComponent,
         ProductsComponent,
+        ProductDetailsComponent,
         NavBarComponent,
         FooterComponent
     ],
-    providers: [ProductDetailsService, ProductDetailsResolver]
+    providers: [ProductDetailsService, ProductsResolver, ProductDetailsResolver]
 })
 export class PublicModule {
 }
