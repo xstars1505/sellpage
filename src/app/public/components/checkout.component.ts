ace961e8ed5d9cc64ca4c5a320a86174705e5dba
Minh Le Tran Ba <baminh.letran@gmail.com>
2017-11-13 17:39:51 +0700

    Add checkout content

---
+++ src/app/public/components/checkout.component.ts
@@ -0,0 +1,25 @@
+import { Component, OnInit } from '@angular/core';
+import {FormBuilder, FormGroup, Validators} from "@angular/forms";
+
+@Component({
+    moduleId: module.id,
+    selector: 'app-checkout',
+    templateUrl: 'checkout.component.html',
+    styleUrls: ['checkout.component.scss']
+})
+export class CheckoutComponent implements OnInit {
+  firstFormGroup: FormGroup;
+  secondFormGroup: FormGroup;
+
+  constructor(private _formBuilder: FormBuilder) { }
+
+  ngOnInit() {
+    this.firstFormGroup = this._formBuilder.group({
+      firstCtrl: ['', Validators.required]
+    });
+    this.secondFormGroup = this._formBuilder.group({
+      secondCtrl: ['', Validators.required]
+    });
+  }
+
+}
